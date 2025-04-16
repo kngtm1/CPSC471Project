0e0d343cbfb774c7b0e74d14428c5ef7eadcfc72
mouna.belhaouas <mouna.belhaouas@ucalgary.ca>
2025-04-16 13:42:53 -0600

    homepage should connect to products now

--- website/views.py
+++ website/views.py
@@ -1,4 +1,5 @@
 #Home page
+import sqlite3
 
 from flask import Blueprint, render_template
 
@@ -6,4 +7,13 @@ views = Blueprint('views',__name__)
 
 @views.route('/')
 def home():
-    return render_template("home.html")
+    connection = sqlite3.connect('website/Data/StoreDB.db')
+    connection.commit()
+    
+    cursors = connection.cursor()
+    cursors.execute("SELECT ProductID, Names, Description, RegistrationDate, Price FROM Product")
+    products = cursors.fetchall()
+    connection.close()
+    #link to customer home page somehow?
+    
+    return render_template("home.html", products=products)
