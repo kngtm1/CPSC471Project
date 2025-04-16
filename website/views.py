48a3ef2f75877028315f1bcb6bf0b6db753074ea
mouna.belhaouas <mouna.belhaouas@ucalgary.ca>
2025-04-16 13:58:12 -0600

    linked orders and orderItem onto views

--- website/views.py
+++ website/views.py
@@ -7,13 +7,24 @@ views = Blueprint('views',__name__)
 
 @views.route('/')
 def home():
+    #connecting to the sql
     connection = sqlite3.connect('website/Data/StoreDB.db')
-    connection.commit()
+    #connection.commit()
+    reader = connection.cursor()
     
-    cursors = connection.cursor()
-    cursors.execute("SELECT ProductID, Names, Description, RegistrationDate, Price FROM Product")
-    products = cursors.fetchall()
+    #get products
+    reader.execute("SELECT ProductID, Names, Description, RegistrationDate, Price FROM Product")
+    products = reader.fetchall()
+    
+    #get orders
+    reader.execute("SELECT OrderID, OrderDate, OrderTotal, Status FROM Orders")
+    orders = reader.fetchall()
+    
+    #get order_item
+    reader.execute("SELECT OrderID, Quantity FROM OrderItem")
+    order_item = reader.fetchall()
+    
     connection.close()
     #link to customer home page somehow?
     
-    return render_template("home.html", products=products)
+    return render_template("home.html", products=products, orders=orders, order_item=order_item)
